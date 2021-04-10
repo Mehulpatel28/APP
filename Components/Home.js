a1ddf52d8b270014c2d1ea8f52238766bccfd578
Mehul Patel <mehulpatel6199@gmail.com>
2021-04-10 13:24:02 +0530

    home

--- Components/Home.js
+++ Components/Home.js
@@ -35,7 +35,7 @@ class Home extends React.Component {
                 >
                 </TextInput>
 
-                <Button title="submit" onPress={()=>{this.submit()}}/>
+                <Button title="press me" onPress={()=>{this.submit()}}/>
             </View>
         )
     }
